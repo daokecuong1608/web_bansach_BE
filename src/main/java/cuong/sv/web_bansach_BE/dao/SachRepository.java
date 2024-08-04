5e774b0563ccedd13959f3a0efe74151bfcd3933
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-08-05 03:28:37 +0700

    BE chi chi tiết sản phẩm

--- src/main/java/cuong/sv/web_bansach_BE/dao/SachRepository.java
+++ src/main/java/cuong/sv/web_bansach_BE/dao/SachRepository.java
@@ -1,10 +1,20 @@
 package cuong.sv.web_bansach_BE.dao;
 
-import cuong.sv.web_bansach_BE.entity.Quyen;
 import cuong.sv.web_bansach_BE.entity.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach, Integer> {
+
+ //RequestParam => lấy dữ liệu từ các tham số truy vấn (query prarameters)
+Page<Sach> findByTenSachContaining(@RequestParam("tenSach")String tenSach , Pageable pageable);
+
+ Page<Sach> findByDanhSachTheLoai_MaTheLoai(@RequestParam("maTheLoai")int maTheLoai , Pageable pageable);
+
+ Page<Sach> findByTenSachContainingAndDanhSachTheLoai_MaTheLoai(@RequestParam("tenSach")String tenSach ,@RequestParam("maTheLoai")int maTheLoai , Pageable pageable);
+
 }
